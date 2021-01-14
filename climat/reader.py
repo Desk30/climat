c9f6b74c6444d35c6974951db495660b91d3d4be
Thibault Chevalérias <tibochev@mailoo.org>
2021-01-14 11:17:25 +0100

    allow for multiple file types, raise TypeError if type is not supported

--- climat/reader.py
+++ climat/reader.py
@@ -7,5 +7,9 @@ def read_data(dirpath: Path, pattern: str) -> Dict[str, xr.Dataset]:
     ds_dict = {}
     for data_file in dirpath.glob(pattern):
         print(f"Reading file {data_file}...")
-        ds_dict[data_file.stem] = xr.open_dataset(data_file, engine="cfgrib")
+        file_extension = data_file.suffix
+        if file_extension == ".grib":
+            ds_dict[data_file.stem] = xr.open_dataset(data_file, engine="cfgrib")
+        else:
+            raise TypeError(f"unsupported data type {file_extension}")
     return ds_dict
